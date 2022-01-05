4bd87c33013e3af62ad4bc1ba9232fb79dd87a4a
tomschie <74539321+tomschie@users.noreply.github.com>
2022-01-05 15:14:34 +0100

    maze change

--- multiple_max_differences.py
+++ multiple_max_differences.py
@@ -5,13 +5,15 @@ from mpc_steer_speed_us import multiple_max_diff
 
 
 def main():
-    iterations = 100
+    iterations = 10
     max_dif_normal = np.zeros(iterations)
     
     for i in range(iterations):
         print("iteration ", i, "started")
         max_diff = multiple_max_diff()
         max_dif_normal[i] = max_diff
+
+    print(max_dif_normal)
 
 
 if __name__ == '__main__':
