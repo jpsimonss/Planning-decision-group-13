f210d3df90bc311efba845848d86e92f3d86f83f
tomschie <74539321+tomschie@users.noreply.github.com>
2022-01-10 10:31:59 +0100

    time

--- multiple_max_differences.py
+++ multiple_max_differences.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 from mpc_steer_speed_us import multiple_max_diff
+from wavefront_test_2 import get_snake
+import time
 
 def main():
     
@@ -15,6 +17,31 @@ def main():
 
     print(max_dif_normal)
 
+def main2():
+    
+    iterations = 100
+    elapsed_time = np.zeros(iterations)
+    
+    for i in range(iterations):
+        print("iteration ", i, "started")
+        start = time.time()
+        snake, array, configuration_space = get_snake(configuration_size=2)
+        end = time.time()
+        elapsed_time[i] = end-start
+
+    print(elapsed_time)
+
+    print("mean =", np.mean(elapsed_time))
+    print("std =", np.std(elapsed_time))
+
+
+
 
 if __name__ == '__main__':
-    main()
+    # main()
+    main2()
+
+
+
+
+
